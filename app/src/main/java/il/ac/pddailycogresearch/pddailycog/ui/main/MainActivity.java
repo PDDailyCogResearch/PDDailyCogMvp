4986ac9926c7a03e730abc84a15ab4fdbe09a29a
Shani <shna080@gmail.com>
2017-11-05 18:09:56 +0200

    Fix Dagger issues

--- app/src/main/java/il/ac/pddailycogresearch/pddailycog/ui/main/MainActivity.java
+++ app/src/main/java/il/ac/pddailycogresearch/pddailycog/ui/main/MainActivity.java
@@ -3,6 +3,9 @@ package il.ac.pddailycogresearch.pddailycog.ui.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +13,14 @@ import java.util.List;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import il.ac.pddailycogresearch.pddailycog.R;
 import il.ac.pddailycogresearch.pddailycog.ui.base.BaseActivity;
 
 
 public class MainActivity extends BaseActivity<MainContract.Presenter> implements MainContract.View {
+    @BindView(R.id.tv_user_info)
+    TextView mTextView;
 
 
     @Override
@@ -24,7 +30,8 @@ public class MainActivity extends BaseActivity<MainContract.Presenter> implement
 
     @Override
     protected void init(@Nullable Bundle state) {
-        getPresenter().loadRssFragments();
+
+       // getPresenter().loadRssFragments();
     }
 
     @Override
@@ -32,9 +39,15 @@ public class MainActivity extends BaseActivity<MainContract.Presenter> implement
         getActivityComponent().inject(this);
     }
 
+    @OnClick(R.id.test_button)
+    public void test(View view){
+        getPresenter().loadRssFragments();
+    }
+
     @Override
     public void onLoadRssFragments() {
         //TODO change name and functionality, also in contract...
+        mTextView.setText("test!");
     }
 
 
